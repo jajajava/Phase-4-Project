27aeba0a4942c5a35992a79fb65447613b6d3645
David <davidstepanov@yahoo.com>
2022-11-14 13:53:46 -0500

    Minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Welcome to the {INSERT NAME} botanical garden! 
+Welcome to the Ashton Botanical Garden! 
 This website provides information about the garden, as well as future events. Sign up today to order tickets or make event reservations.
 
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  resources :reservations, only: [:post, :destroy]
-  resources :users, only: [:post, :patch, :delete]
+  resources :reservations, only: [:create, :destroy]
+  resources :users, only: [:create, :patch, :delete]
   resources :events
   post "/auth/login", to: "auth#login"
   get "/me", to: "users#me"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
