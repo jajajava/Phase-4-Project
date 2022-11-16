e017cf3ff2fb4fa9edb8611dd84e451fc8e1c69f
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-11-16 15:33:38 -0500

    updated plants page

--- frontend/src/components/Plants.js
+++ frontend/src/components/Plants.js
@@ -4,10 +4,90 @@ function Plants() {
     return (
         <div className="events">
             <h1 className="title">Plants</h1>
-        <div className="grid-container">
-                    
+            <div className="grid-container2">
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Amaryllis</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div>  
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div> 
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div>      
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div> 
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div> 
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div> 
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div> 
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div> 
+
+                <div className='plant-container'>
+                    <img src="https://i.postimg.cc/SKGhkdMD/4F34F4.png" alt='image'></img>
+                    <h1>Name</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut distinctio quasi, similique, impedit autem eos corporis molestias quo minima possimus repellat veritatis ex modi magni quaerat quibusdam, enim voluptate.</p>
+                </div>          
+            </div>
+            
+            <hr></hr>
+
+        <div className="intro-footer">
+                <div className="info-container">
+                    <h1>Location</h1>
+                    <p>1842 Jarvisville Road<br></br>
+                    Brooklyn, NY 10458-5126</p>    
+                </div>
+                <div className="info-container">
+                    <h1>Hours</h1>
+                    <p><span>Mon - Fri</span> : 10am - 6pm</p>
+                    <p><span>Sat - Sun</span> : 9am - 8pm</p>    
+                </div>
+                <div className="info-container">
+                    <h1>Contact</h1>
+                    <p>718 - 903 - 3998<br></br>
+                    ashton@info.com</p>    
+                </div>
+                <div className="icons">
+                    <i className='bx bxl-facebook'></i>
+                    <i className='bx bxl-instagram-alt' ></i>
+                    <i className='bx bxl-twitter' ></i>
+                    <i className='bx bxl-linkedin-square' ></i>
+                </div>
+            </div>
         </div>
-        </div>
+        
+        
     )
 }
 
